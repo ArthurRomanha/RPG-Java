2ea62f843c3193571ca633550de7271bf418166e
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-10-02 07:07:57 -0300

    Add files via upload

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -30,16 +30,16 @@ public class Player extends Entity {
     public void getPlayerImage() {
         try {
             up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_up_2.png"));
 
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_down_2.png"));
 
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_left_2.png"));
 
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/boy_right_2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,7 +59,7 @@ public class Player extends Entity {
             direction = "right";
             posX += speed;
         }
-        spriteCounter++;
+        timerChangeSprite++;
         if (timerChangeSprite > 12) {
             if (spriteCounter == 1) {
                 spriteCounter = 2;
@@ -109,6 +109,8 @@ public class Player extends Entity {
                 break;
         }
         System.out.println(down1);
+        System.out.println(direction);
+        System.out.println(spriteCounter);
         g2.drawImage(image, posX, posY, gs.tileSize, gs.tileSize, null);
     }
 }
